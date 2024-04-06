b99b916c3c203035621ae536f384a955947938ca
Aviral Srivastava <81246801+meaviral17@users.noreply.github.com>
2024-04-06 12:49:47 +0530

    MUI Configurations completed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,15 @@
-
-
+import { createTheme } from "@mui/system"
+import { useMemo } from "react"
+import { CssBaseline, ThemeProvider } from "@mui/material"
+import { themeSettings } from "./theme"
 function App() {
-
+  const theme= useMemo(() => createTheme(themeSettings), [])
   return (
-    <div className='App'>
+    <div className='app'>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        hi
+      </ThemeProvider>
     </div>
   )
 }
